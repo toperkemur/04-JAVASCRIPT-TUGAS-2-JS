1fb967f1a298b6bdd51af6a167b674baf05f32d5
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-04-04 15:03:27 +0700

    Initial Commit
    Menambahkan tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var jordan = 23;
+var nama = "Michael Jordan";
+var juara = true;
+var kosong;
+var nyata = null;
+var tahunJuara = [1991,1992,1993,1996,1997,1998];
+
+console.log(jordan);
+console.log(nama);
+console.log(juara);
+console.log(kosong);
+console.log(nyata);
+console.log(tahunJuara);
